ddbdf0ad438ae3f0bd541935de42f85b93534a35
Angeloamenta <angeloamenta7@gmail.com>
2022-05-24 19:52:57 +0200

    add array title

--- src/views/DrinkView.vue
+++ src/views/DrinkView.vue
@@ -1,13 +1,59 @@
 <template>
 <div>
     Drink
+      <ul>
+         <li v-for="(drink, index) in drinks" :key="index">
+         <h3>{{drink.nome}}</h3>
+         <h5>Condimenti</h5>
+         <p>{{drink.condimenti}}</p>
+         <div>€ {{drink.prezzo}}</div>
+         </li> 
+      </ul>
 </div>
   
 </template>
 
 <script>
 export default {
-name:"DrinkVue"
+name:"DrinkVue",
+data() { return {
+
+    drinks: [
+        // nostri
+        {
+            nome: "Blue summer",
+            categoria: "nostri",
+            condimenti: "(lime, cordial lime & blu curacao, rum blanco)",
+            prezzo: 8
+        },
+        {
+            nome: "Green Woods",
+            categoria: "nostri",
+            condimenti: "(lime,zucchero all’alloro, melone,gin, dash di midori)",
+            prezzo: 8
+        },
+        {
+            nome: "Strawberry Basil...Forever",
+            categoria: "nostri",
+            condimenti: "(zucchero,lime, fragole, vodka al basilico, soda al pompelmo)",
+            prezzo: 8
+        },
+        {
+            nome: "Butter Trid",
+            categoria: "nostri",
+            condimenti: "(limone,ananas,bourbon al burro, dash di angostura)",
+            prezzo: 8
+        },
+                {
+            nome: "Fresh Alexander",
+            categoria: "nostri",
+            condimenti: "(limone,orzata,brandy,stillabunt,bitter al cioccolato, ginger beer)",
+            prezzo: 8
+        },
+    ]
+
+}
+}
 }
 </script>
 
--- src/views/PranzoView.vue
+++ src/views/PranzoView.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-      Insalate
+      <div class="nav-2">
+          <div>Cosa cerchi?</div>
+          <span @click="setAll()">all</span> <span @click="setInsalata()">insalate</span> <span @click="setPiatti()">piatti</span>
+      </div>
       <ul>
-         <li v-for="(piatto, index) in piatti" :key="index">
+         <li v-for="(piatto, index) in piatti2" :key="index">
+         <h1>{{piatto.title}}</h1>
          <h3>{{piatto.nome}}</h3>
          <h5>Condimenti</h5>
          <p>{{piatto.condimenti}}</p>
@@ -16,12 +20,18 @@
 export default {
 name: "PranzoView",
 data() { return {
+    
+    setter : "All",
+    piatti2: [],
     piatti: [
+
+
         {
             nome: "Bresaola",
             categoria: "insalata",
             condimenti: "iceberg, radicchio,valeriana, datterino, grana, bresaola",
-            prezzo: 10
+            prezzo: 10,
+            title: "Insalate"
         },
         {
             nome: "Tonno",
@@ -31,17 +41,70 @@ data() { return {
         },
         {
             nome: "Greca",
-            categoria: "insalata",
+            categoria: "piatti",
             condimenti: "iceberg,cipolla,olive,cetriolo,datterino,feta,salsa yogurt",
+            prezzo: 8,
+            title: "I Nostri piatti"
+        },
+
+        {
+            nome: "Caprese",
+            categoria: "piatti",
+            condimenti: "datterino, fior di latte, basilico",
             prezzo: 8
         },
     ]
 
 }
+},
+mounted() {
+    this.piatti2 =  this.piatti.filter((element) => element.categoria == this.setter)
+    console.log("new", this.piatti2);
+    
+    if ( this.setter == "All") {
+     this.piatti2 = this.piatti;
+    }
+},
+
+methods: {
+    setAll() {
+        this.setter = "All";
+            this.piatti2 =  this.piatti.filter((element) => element.categoria == this.setter)
+    console.log("new", this.piatti2);
+    
+    if ( this.setter == "All") {
+     this.piatti2 = this.piatti;
+    }
+    },
+
+    setInsalata() {
+        this.setter = "insalata";
+            this.piatti2 =  this.piatti.filter((element) => element.categoria == this.setter)
+    console.log("new", this.piatti2);
+    
+    if ( this.setter == "All") {
+     this.piatti2 = this.piatti;
+    }
+    },
+
+      setPiatti() {
+        this.setter = "piatti";
+            this.piatti2 =  this.piatti.filter((element) => element.categoria == this.setter)
+    console.log("new", this.piatti2);
+    
+    if ( this.setter == "All") {
+     this.piatti2 = this.piatti;
+    }
+    },
+
 }
 }
 </script>
 
-<style>
+<style lang="scs" scoped>
 
-</style>
+
+
+</style>>
+
+
