9c4902f47a6538340769b64a2d1f8f0d9e342b3e
Angeloamenta <angeloamenta7@gmail.com>
2022-05-25 12:09:59 +0200

    cena ok

--- src/views/CenaView.vue
+++ src/views/CenaView.vue
@@ -1,14 +1,37 @@
 <template>
-  <div>
-      pizze
-      <ul>
-         <li v-for="(piatto, index) in piatti" :key="index">
-         <h3>{{piatto.nome}}</h3>
-         <h5>Condimenti</h5>
-         <p>{{piatto.condimenti}}</p>
-         <div>€ {{piatto.prezzo}}</div>
-         </li> 
-      </ul>
+    <div class="">
+      <div class="nav-2 container-fluid">
+          <div class="row">
+          <div class="col-12">Cosa cerchi?</div>
+          <div class="col-12 d-flex justify-content-center">
+          <ul class="list-unstyled d-flex justify-content-between w-50">
+            <li @click="setAll()" class="">Tutto</li>
+            <li @click="setPizzeClassiche()" class="">Pizze Classiche</li>
+            <li @click="setPizzeSpeciali()" class="">Pizze Speciali</li>
+            <li @click="setPizzeMare()">Pizze di Mare</li>
+            <li @click="setPiattiUnici()">Piatti Unici</li>
+            <li @click="setFrigitoria()">Frigitoria</li>
+         </ul>
+          </div>
+          </div>
+      </div>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col">
+                    <ul>
+                        <li v-for="(piatto, index) in piatti2" :key="index">
+                        <h1>{{piatto.title}}</h1>
+                        <h3>{{piatto.nome}}</h3>
+                        <h5 v-if="piatto.condimenti == null"></h5>
+                        <h5 v-else>Condimenti</h5>
+                        <p>{{piatto.condimenti}}</p>
+                        <div>€ {{piatto.prezzo}}</div>
+                        </li> 
+                    </ul>
+              </div>
+          </div>
+
+      </div>
   </div>
 </template>
 
@@ -16,6 +39,8 @@
 export default {
 name:"CenaView",
 data() { return {
+    setter : "All",
+    piatti2: [],
     piatti: [
 
         // pizze clasiche
@@ -41,16 +66,122 @@ data() { return {
 
         // pizze speciali
 
+        {
+            nome: "sapori siculi ",
+            categoria: "pizza speciale",
+            condimenti: "salsa di pomodoro, mozzarella, salsiccia fresca, finocchietto selvatico, mollica condita, olio",
+            prezzo:10,
+            title: "Pizze Speciali"
+        },
+        {
+            nome: "Carciofina",
+            categoria: "pizza speciale",
+            condimenti: "mozzarella, crema di carciofi, speck, datterino",
+            prezzo:10,
+
+        },
 
         // pizze di mare
 
+        {
+            nome: "la spiaggetta ",
+            categoria: "pizza mare",
+            condimenti: "salsa di pomodoro, polipo*, gamberetti*, cozze*,gamberoni*, prezzemolo, olio",
+            prezzo:10,
+            title: "Pizze Di Mare"
+        },
 
         // piatti unici
 
+        {
+            nome: "IL VEGETARIANO",
+            categoria: "piatti unici",
+            condimenti: "datterino confit, radicchio grigliato, melanzana grigliata, burrata di bufala, basilico,crostoni, olio evo",
+            prezzo:13,
+            title: "Piatti unici"
+        },
+
         // frigitoria
+
+                {
+            nome: "partatine fritte",
+            categoria: "frigitoria",
+            prezzo:4,
+            title: "Frigitoria"
+        },
     ]
 
 }
+},
+mounted() {
+    this.piatti2 =  this.piatti.filter((element) => element.categoria == this.setter)
+    console.log("new", this.piatti2);
+    
+    if ( this.setter == "All") {
+     this.piatti2 = this.piatti;
+    }
+},
+
+methods: {
+    setAll() {
+        this.setter = "All";
+            this.piatti2 =  this.piatti.filter((element) => element.categoria == this.setter)
+    console.log("new", this.piatti2);
+    
+    if ( this.setter == "All") {
+     this.piatti2 = this.piatti;
+    }
+    },
+
+    setPizzeClassiche() {
+        this.setter = "pizza";
+            this.piatti2 =  this.piatti.filter((element) => element.categoria == this.setter)
+    console.log("new", this.piatti2);
+    
+    if ( this.setter == "All") {
+     this.piatti2 = this.piatti;
+    }
+    },
+
+    setPizzeSpeciali() {
+        this.setter = "pizza speciale";
+            this.piatti2 =  this.piatti.filter((element) => element.categoria == this.setter)
+    console.log("new", this.piatti2);
+    
+    if ( this.setter == "All") {
+     this.piatti2 = this.piatti;
+    }
+    },
+
+    setPizzeMare() {
+        this.setter = "pizza mare";
+            this.piatti2 =  this.piatti.filter((element) => element.categoria == this.setter)
+    console.log("new", this.piatti2);
+    
+    if ( this.setter == "All") {
+     this.piatti2 = this.piatti;
+    }
+    },
+
+    setPiattiUnici() {
+        this.setter = "piatti unici";
+            this.piatti2 =  this.piatti.filter((element) => element.categoria == this.setter)
+    console.log("new", this.piatti2);
+    
+    if ( this.setter == "All") {
+     this.piatti2 = this.piatti;
+    }
+    },
+
+     setFrigitoria() {
+        this.setter = "frigitoria";
+            this.piatti2 =  this.piatti.filter((element) => element.categoria == this.setter)
+    console.log("new", this.piatti2);
+    
+    if ( this.setter == "All") {
+     this.piatti2 = this.piatti;
+    }
+    },
 }
 }
 </script>
