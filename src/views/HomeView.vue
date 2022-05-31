c7ed7d1abde1dc68b5122fd637a402b001c35e95
Angeloamenta <angeloamenta7@gmail.com>
2022-05-31 11:08:10 +0200

    add logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="home container-fluid">
     <div class="row home-row">
-      <div class="col d-flex justify-content-center align-items-center">
-        <div class="icon">
-          <img src="../assets/spiaggettalogo.svg" alt="">
-        </div>
+      <div class="col-12 d-flex justify-content-start align-items-center logo-container">
+          <img class="logo-spiaggetta" src="../assets/spiaggettalogo.svg" alt="">
+          <img class="logo-cuore" src="../assets/cuore spiaggetta.gif" alt="">
       </div>
     </div>
 
@@ -25,14 +24,38 @@ export default {
 
 <style>
 
+.home {
+  background-color: #2F3A2B;
+}
+
 
 .icon {
   width: 300px;
   height: 300px;
 }
 
-.home-row {
+/* .home-row {
   height: 80vh;
+} */
+
+.logo-container {
+  position: relative;
+  height: 20vh;
+}
+
+.logo-spiaggetta {
+  position: absolute;
+  top: 35px;
+  left: 20px;
+  height: 50px;
+}
+
+.logo-cuore {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 150px;
+
 }
 
 </style>
