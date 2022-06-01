e827cb2fa56034c560c79d8f7b89c7ab84affa3f
Angeloamenta <angeloamenta7@gmail.com>
2022-06-01 15:03:32 +0200

    add switch show

--- src/components/PreFooter.vue
+++ src/components/PreFooter.vue
@@ -2,9 +2,9 @@
   <section>
     <div class="container-fluid menu-bg m-0">
       <ul>
-          <li>Coperto: 2.5</li>
+          <li v-show="switchShow">Coperto: 2.5</li>
           <li>Se sapete di essere allercigi o intolleranti a determinati alimenti, vi preghiamo di comunicarlo al nostro personale prima di effettuare la vostra ordinazione</li>
-          <li>N.B. supplemento da €1,00 a €3,50</li>
+          <li v-show="switchShow">N.B. supplemento da €1,00 a €3,50</li>
       </ul>
     </div>
   </section>
@@ -12,7 +12,19 @@
 
 <script>
 export default {
-name:"PreFooter"
+name:"PreFooter",
+data() {return {
+  switchShow: true
+}
+},
+mounted() {
+    console.log("test",window.location.hash)
+    if (window.location.hash == "#/Drink") {
+      this.switchShow = false 
+    }
+      console.log("show",this.switchShow);
+}
+
 }
 </script>
 
