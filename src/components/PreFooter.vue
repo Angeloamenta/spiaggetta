f5f89a7d650d76ef09a0f93f15b7f18c36c156d7
Angeloamenta <angeloamenta7@gmail.com>
2022-06-01 11:50:41 +0200

    scritta allergeni

--- src/components/PreFooter.vue
+++ src/components/PreFooter.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid menu-bg m-0">
       <ul>
           <li>Coperto: 2.5</li>
-          <li>Allergeni</li>
+          <li>Se sapete di essere allercigi o intolleranti a determinati alimenti, vi preghiamo di comunicarlo al nostro personale prima di effettuare la vostra ordinazione</li>
           <li>N.B. supplemento da €1,00 a €3,50</li>
       </ul>
     </div>
